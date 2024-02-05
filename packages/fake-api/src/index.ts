8c8e1fa05fad9922e80a5bda211083bdd2d82551
Tomasz Bajda <tomasz.bajda@acaisoft.com>
2024-02-05 01:19:54 +0100

    add lerna

---
+++ packages/fake-api/src/index.ts
@@ -0,0 +1,39 @@
+import express from "express";
+import cors from "cors";
+
+import colors from "colors";
+import { faker } from "@faker-js/faker";
+import { Specialist } from "./types";
+import { CALAMARI } from "./calamari";
+
+const app = express();
+
+const PORT = 3000;
+
+app.use(cors());
+
+colors.enable();
+
+const createRandomUser = (): Specialist => ({
+  id: faker.string.uuid(),
+  firstName: faker.person.firstName(),
+  lastName: faker.person.firstName(),
+  avatar: faker.image.avatar(),
+  specialization: faker.person.jobTitle(),
+  votes: faker.helpers.arrayElements([1, 2, 3, 4, 5], { min: 0, max: 7 }),
+  userVote: faker.number.int({ min: 0, max: 5 }),
+  isFavorite: false,
+});
+
+const USERS = faker.helpers.multiple(createRandomUser, {
+  count: 4,
+});
+
+app.get("/specialists", (req, res) => {
+  res.send(USERS);
+});
+
+app.listen(PORT, () => {
+  console.log(CALAMARI.green);
+  console.log(`Listening on port ${PORT}`.green);
+});
---
+++ packages/ui/src/services/types.ts
@@ -0,0 +1,10 @@
+export type Specialist = {
+  id: string;
+  firstName: string;
+  lastName: string;
+  avatar: string;
+  specialization: string;
+  votes: number[];
+  userVote: number;
+  isFavorite: boolean;
+};
